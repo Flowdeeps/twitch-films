60eff44c0ddd2044a4cd3bf7f94ce442751c93f0
Flowdeeps <speedwolf+github@gmail.com>
2024-02-02 01:13:50 +0100

    I'm a fool and accidentally committed a broken merge

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,12 @@
 window.addEventListener("load", function() {
-
+  var httpRequest
   var data = {}
-
-  function callList(input) {
-
-    let list = ""
-
-    if (input[0]) {
-      list = "adverts"
-    } else if (input[1]) {
-      list = "cartoons"
-    } else if (input[2]) {
-      list = "movies"
-    } else {
-      console.log("nope")
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest()
+    if (!httpRequest) {
+      console.log("Giving up :( Cannot create an XMLHTTP instance")
       return false
     }
-<<<<<<< HEAD
-
-    var httpRequest
-    function makeRequest() {
-      httpRequest = new XMLHttpRequest()
-      if (!httpRequest) {
-        console.log("Giving up :( Cannot create an XMLHTTP instance")
-        return false
-      }
-      httpRequest.onreadystatechange = getContents
-      httpRequest.open("GET", "js/" + list + ".json")
-      httpRequest.send()
-    };
-    makeRequest()
-    
-    function getContents() {
-      try {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-            data = {}
-            data = JSON.parse(httpRequest.response)
-          } else {
-            console.log("There was a problem with the request.")
-          }
-=======
     httpRequest.onreadystatechange = getContents
     httpRequest.open("GET", "js/playlist.json")
     httpRequest.send()
@@ -53,41 +19,14 @@ window.addEventListener("load", function() {
           data = shuffle(JSON.parse(httpRequest.response))
         } else {
           console.log("There was a problem with the request.")
->>>>>>> 184976d (Added a lot of films and removed a couple due to rights complaints from Twitch. I'm trying to only use films from this list: https://archive.org/details/feature_films?tab=collection&page=2&and%5B%5D=subject%3A%22sci-fi%22&and%5B%5D=subject%3A%22Sci-Fi%22&and%5B%5D=subject%3A%22sci+fi%22&and%5B%5D=subject%3A%22scifi%22 that are marked as Public Domain)
         }
       }
-      catch (e) {
-        console.log("Caught Exception: " + e.description)
-      }
+    }
+    catch (e) {
+      console.log("Caught Exception: " + e.description)
     }
   }
 
-<<<<<<< HEAD
-  // advert, cartoon, feature
-  let playState = [1, 0, 0]
-
-  function advancePlayState() {
-    console.log(playState)
-    if (playState[0]) {
-      playState = []
-      playState = [0, 1, 0]
-    }
-    else if (playState[1]) {
-      playState = []
-      playState = [0, 0, 1]
-    }
-    else if (playState[2]) {
-      playState = []
-      playState = [1, 0, 0]
-    }
-    else {
-      playState = []
-      playState = [0, 1, 0]
-    }
-  }
-  
-  callList(playState)
-=======
   // this is a Fisher-Yates or Knuth shuffle
   function shuffle(array) {
     let currentIndex = array.length, randomIndex;
@@ -108,7 +47,6 @@ window.addEventListener("load", function() {
   }
 
   let index = 0;
->>>>>>> 184976d (Added a lot of films and removed a couple due to rights complaints from Twitch. I'm trying to only use films from this list: https://archive.org/details/feature_films?tab=collection&page=2&and%5B%5D=subject%3A%22sci-fi%22&and%5B%5D=subject%3A%22Sci-Fi%22&and%5B%5D=subject%3A%22sci+fi%22&and%5B%5D=subject%3A%22scifi%22 that are marked as Public Domain)
 
   const video = document.createElement("video")
   video.autoplay = true
@@ -116,9 +54,16 @@ window.addEventListener("load", function() {
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
 
+  function addIndex() {
+    if (index < data.length) {
+      index += 1
+    } else {
+      index = 0
+    }
+  }
+
   function playVideo() {
-    video.src = data[getRand(data.length)].url
-    console.log(video.src)
+    video.src = data[index].url
     video.addEventListener("loadeddata", function () {
       video.play()
       video.width = video.videoWidth
@@ -131,9 +76,8 @@ window.addEventListener("load", function() {
       playMe()
     })
     video.addEventListener("ended", function () {
+      addIndex()
       closeCurtains()
-      advancePlayState()
-      callList(playState)
       playVideo()
     })
   }
@@ -145,10 +89,8 @@ window.addEventListener("load", function() {
 
   document.getElementById("next").addEventListener("click", function (e) {
     e.preventDefault()
-    video.pause()
+    addIndex()
     closeCurtains()
-    advancePlayState()
-    callList(playState)
     playVideo()
   })
 
@@ -198,11 +140,6 @@ window.addEventListener("load", function() {
     playMe()
   })
 
-  function getRand(input) {
-    let output = Math.floor(Math.random() * input)
-    return output
-  }
-
   function openCurtains() {
     let curtains = document.getElementsByClassName("curtain")
     for (let i = 0; i < curtains.length; i++) {
@@ -244,9 +181,5 @@ window.addEventListener("load", function() {
     repositionMe()
   })
 
-<<<<<<< HEAD
-})
-=======
   makeRequest()
 })
->>>>>>> 184976d (Added a lot of films and removed a couple due to rights complaints from Twitch. I'm trying to only use films from this list: https://archive.org/details/feature_films?tab=collection&page=2&and%5B%5D=subject%3A%22sci-fi%22&and%5B%5D=subject%3A%22Sci-Fi%22&and%5B%5D=subject%3A%22sci+fi%22&and%5B%5D=subject%3A%22scifi%22 that are marked as Public Domain)
